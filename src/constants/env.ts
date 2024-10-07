a324f65498abb70497b04f9ebf1375ff1738ae38
@r.shageev <radmir1118@gmail.com>
2024-10-07 20:32:07 +0500

    feat(app): add support db

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,13 +1,13 @@
-const DB_USER = '';
+const DB_USER = process.env.DB_USER || '';
 const DB_PASSWORD = process.env.DB_PASSWORD || '';
 
-const DB_HOST = 'localhost';
-const DB_PORT = '5300';
-const DB_NAME = 'postgres';
+const DB_HOST = process.env.DB_HOST || '';
+const DB_PORT = process.env.DB_PORT || '';
+const DB_NAME = process.env.DB_NAME || '';
 
 export const DB_CONNECTION = (() => {
   const password = DB_PASSWORD ? `:${DB_PASSWORD}` : '';
   const user = DB_USER ? `${DB_USER}${password}@` : '';
 
-  return `postgresql://${user}${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+  return `postgresql://${user}${DB_HOST}:${DB_PORT}/${DB_NAME}?`;
 })();
