425811a5d1c24847e62010b427bc367cd300f87b
@r.shageev <radmir1118@gmail.com>
2024-10-07 23:05:22 +0500

    feat(app): add migration

---
+++ migrate.js
@@ -0,0 +1,25 @@
+require('./build/plugins/env');
+const path = require('path');
+const { runner } = require('node-pg-migrate');
+
+const migrationDir = path.join(__dirname, '/migrations');
+
+const { DB_CONNECTION } = require('./build/constants/env');
+
+const runMigrations = async () => {
+  const options = {
+    databaseUrl: DB_CONNECTION,
+    dir: migrationDir,
+    direction: 'up',
+    count: Infinity,
+  };
+  try {
+    console.log('Run migrations');
+    await runner(options);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+};
+
+runMigrations();
